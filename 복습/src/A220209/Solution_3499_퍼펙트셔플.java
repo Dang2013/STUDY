e53d495bb404ca039635d39d2845952c82c2db82
Dang2013 <dang2013@naver.com>
2022-02-21 21:35:52 +0900

    220221

--- 복습/src/A220209/Solution_3499_퍼펙트셔플.java
+++ 복습/src/A220209/Solution_3499_퍼펙트셔플.java
@@ -14,40 +14,52 @@ public class Solution_3499_퍼펙트셔플 {
 		for (int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt();
 			
+			// 카드 입력
 			String[] deck = new String[N];
-			
 			for (int i = 0; i < N; i++) {
 				deck[i] = sc.next();
 			}
 			
 			sb.append("#"+tc+" ");
 			
+			// 카드 수가 짝수이면
 			if(N % 2 == 0) {
 				int idx = 0;
 				int cnt = 0;
 				while(cnt < N) {
 					sb.append(deck[idx]+" ");
+					// 짝수 순번엔
 					if(cnt % 2 == 0) {
+						// 다음 인덱스가 카드 수의 절반만큼 증가
 						idx += (N/2);
 					}
+					// 홀수 순번엔
 					else {
+						// 다음 인덱스가 (카드 수의 절반 - 1)만큼 감소 
 						idx -= ((N/2)-1);
 					}
+					// 다음 순번으로
 					cnt++;
 				}
 				sb.append("\n");
 			}
+			// 카드 수가 홀수이면
 			else {
 				int idx = 0;
 				int cnt = 0;
 				while(cnt < N) {
 					sb.append(deck[idx]+" ");
+					// 짝수 순번엔
 					if(cnt % 2 ==0 ) {
+						// 다음 인덱스가 (카드 수의 절반 + 1)만큼 증가
 						idx += (N/2)+1;
 					}
+					// 홀수 순번엔
 					else {
+						// 다음 인덱스가 카드 수의 절반만큼 감소
 						idx -= (N/2);
 					}
+					// 다음 순번으로
 					cnt++;
 				}
 				sb.append("\n");
